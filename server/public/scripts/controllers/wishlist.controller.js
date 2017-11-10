0242e17fbb3dec1fb5557ac67e38c6bfb0e08fef
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-11-10 11:20:55 -0600

    bought items on wishlist page drops down and shows items thatve been bought

--- server/public/scripts/controllers/wishlist.controller.js
+++ server/public/scripts/controllers/wishlist.controller.js
@@ -5,56 +5,100 @@ myApp.controller('WishController', ['UserService', 'ListService', '$routeParams'
     self.userService = UserService;
     self.currentList = ListService.currentList;
     ListService.getList($routeParams.id);
-    ListService.getLists(self.userService.userObject.userId);
-    // user authentication for later
-    // UserService.getuser();
-    // console.log('UserService userObject: ', UserService.userObject);
-    
-    // self.userObject = {};
-    // self.getuser = () => {
-    //     $http.get('/user').then(function (response) {
-    //         if (response.data.username) {
-    //             self.userObject.userId = response.data.id;
-    //             self.userObject.userName = response.data.username;
-    //             //Call for wishlists to populate dropdown
-    //             //ListService.getLists(self.userObject.userId);
-    //         } else {
-    //             console.log('Currently not logged in.');
-    //         }
-    //     });
-    // }
-    self.wishlists = ListService.wishlists;
+
+
+    if (self.userService.userObject.userId) {
+        ListService.getLists(self.userService.userObject.userId);
+        self.wishlists = ListService.wishlists;
+
+        // handling items
+        self.addItem = (item) => {
+            // supposed to close email list if its already opened
+            if (self.item.name === undefined || self.item.description === undefined) {
+                swal({
+                    title: "You need a name AND a description!",
+                    type: 'warning'
+                })
+            } else {
+                // shoving events to handle
+                item.list = $routeParams.id;
+                item.bought = false;
+                item.secret = false;
+                // log checker
+                //console.log('Item to send to server: ', item);
+                // function
+                self.inputDiv = 'Default';
+                swal({
+                    title: "Item Added",
+                    type: 'info'
+                })
+                ListService.addItem(item);
+                self.item = {};
+            }
+        }
+
+        self.deleteItem = (item) => {
+            item.list = $routeParams.id;
+            //console.log('Delete item button is clicked!');
+            swal({
+                title: 'Item Deleted',
+                type: 'info',
+            });
+            ListService.deleteItem(item);
+        }
+        // end handling items
+
+        // handling delete current list
+        self.deleteCurrentList = () => {
+            //console.log('Current list delete!!');
+            swal({
+                title: 'Are you sure you want to delete this list??',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: "Oops! No Way!"
+            }).then(function () {
+                swal(
+                    'Deleted!',
+                    'Your list has been deleted.',
+                    'success'
+                )
+                ListService.deleteCurrentList($routeParams.id);
+            }, function (dismiss) {
+                if (dismiss === 'cancel') {
+                    swal(
+                        'Cancelled!',
+                        'Your list is safe! ... For now.',
+                        'info'
+                    )
+                }
+            });
+        }
+        //
+
+        // email current list
+        self.emailList = (emails) => {
+            console.log('Email button clicked, sending email(s): ', emails);
+            //Fix this later for heroku
+            emails.link = 'https://prime-solo-wishlist.herokuapp.com/#/wishlist/' + $routeParams.id;
+            emails.user = self.userService.userObject.userName;
+            self.inputDiv = 'Default';
+            swal({
+                title: 'List is being sent!',
+                type: 'info'
+            })
+            ListService.emailList(emails);
+            self.emails = {};
+        }
+        //
+    }
 
     self.login = () => {
         $location.path('/home');
     }
     // end user auth
-
-    // handling items
-    self.addItem = (item) => {
-        // supposed to close email list if its already opened
-        if (self.item.name === undefined || self.item.description === undefined) {
-            swal({
-                title: "You need a name AND a description!",
-                type: 'warning'
-            })
-        } else {
-            // shoving events to handle
-            item.list = $routeParams.id;
-            item.bought = false;
-            item.secret = false;
-            // log checker
-            //console.log('Item to send to server: ', item);
-            // function
-            self.inputDiv = 'Default';
-            swal({
-                title: "Item Added",
-                type: 'info'
-            })
-            ListService.addItem(item);
-            self.item = {};
-        }
-    }
 
     self.updateItem = (itemEdit) => {
         itemEdit.list = $routeParams.id;
@@ -62,78 +106,17 @@ myApp.controller('WishController', ['UserService', 'ListService', '$routeParams'
         ListService.editItem(itemEdit);
     }
 
-
-    self.deleteItem = (item) => {
-        item.list = $routeParams.id;
-        //console.log('Delete item button is clicked!');
-        swal({
-            title: 'Item Deleted',
-            type: 'info',
-        });
-        ListService.deleteItem(item);
-    }
-    // end handling items
-
-    // handling delete current list
-    self.deleteCurrentList = () => {
-        //console.log('Current list delete!!');
-        swal({
-            title: 'Are you sure you want to delete this list??',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
-            cancelButtonText: "Oops! No Way!"
-        }).then(function () {
-            swal(
-                'Deleted!',
-                'Your list has been deleted.',
-                'success'
-            )
-            ListService.deleteCurrentList($routeParams.id);
-        }, function (dismiss) {
-            if (dismiss === 'cancel') {
-                swal(
-                    'Cancelled!',
-                    'Your list is safe! ... For now.',
-                    'info'
-                )
-            }
-        });
-    }
-    //
-
-    // email current list
-    self.emailList = (emails) => {
-        console.log('Email button clicked, sending email(s): ', emails);
-        //Fix this later for heroku
-        emails.link = 'https://prime-solo-wishlist.herokuapp.com/#/wishlist/' + $routeParams.id;
-        emails.user = self.userService.userObject.userName;
-        self.inputDiv = 'Default';
-        swal({
-            title: 'List is being sent!',
-            type: 'info'
-        })
-        ListService.emailList(emails);
-        self.emails = {};
-    }
-    //
-
     // anchor's away!
-    self.backToTop = () =>{
+    self.backToTop = () => {
         $window.scrollTo(0, 0);
     }
 
     // handles sorting functions
     self.itemSorter = 'name';
     self.reverse = false;
-  
-    self.sortBy = function(itemSorter) {
-      self.reverse = (self.itemSorter === itemSorter) ? !self.reverse : true;
-      self.itemSorter = itemSorter;
-    };
 
-    //check user, check lists
-    // UserService.getuser();
+    self.sortBy = function (itemSorter) {
+        self.reverse = (self.itemSorter === itemSorter) ? !self.reverse : true;
+        self.itemSorter = itemSorter;
+    };
 }]);
--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -48,8 +48,8 @@ router.post('/', function (req, res) {
     if (req.isAuthenticated()) {
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-                return console.log(error);
                 res.sendStatus(500);
+                return console.log(error);
             }
             // console.log('Message %s sent: %s', info.messageId, info.response);
             console.log('messageId:', info.messageId);
